0a6c54cf4f017033bcb2723012ecc7c642824d3b
Chiisom <Chiisom03@gmail.com>
2022-09-24 09:49:29 +0100

    Implememted search

--- pages/index.vue
+++ pages/index.vue
@@ -32,7 +32,84 @@
 
     <!-- Movies -->
     <div class="py-8 px-4">
+      <!-- search result -->
       <div
+        v-if="searchInput !== ''"
+        id="movie-grid"
+        class="grid gap-x-8 gap-y-16 grid-cols-1 md:grid-cols-3 lg:grid-cols-4"
+      >
+        <div
+          class="relative flex flex-col"
+          v-for="(movie, idx) in searchedMovies"
+          :key="idx"
+        >
+          <div class="relative overflow-hidden text-white group">
+            <img
+              :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+              class="block h-full w-full"
+              alt=""
+            />
+            <p
+              class="
+                absolute
+                top-0
+                left-0
+                flex
+                justify-center
+                items-center
+                w-10
+                h-10
+                bg-touch
+              "
+            >
+              {{ movie.vote_average }}
+            </p>
+            <p
+              class="
+                leading-normal
+                absolute
+                bottom-0
+                bg-touch
+                p-3
+                transform
+                translate-y-full
+                transition-all
+                ease-in-out
+                group-hover:opacity-100 group-hover:translate-y-0
+              "
+            >
+              {{ movie.overview }}
+            </p>
+          </div>
+          <div class="mt-auto">
+            <p class="text-white mt-2 text-sm">
+              {{ movie.title.slice(0, 25) }}
+              <span v-if="movie.title.length > 25">...</span>
+            </p>
+            <p class="my-2 text-white">
+              Released:
+              {{
+                new Date(movie.release_date).toLocaleDateString("en-us", {
+                  month: "long",
+                  day: "numeric",
+                  year: "numeric",
+                })
+              }}
+            </p>
+            <NuxtLink
+              class="btn"
+              :to="{
+                name: 'movies-movieID',
+                params: { movieID: movies.id },
+              }"
+              >Get More Info</NuxtLink
+            >
+          </div>
+        </div>
+      </div>
+
+      <div
+        v-else
         id="movie-grid"
         class="grid gap-x-8 gap-y-16 grid-cols-1 md:grid-cols-3 lg:grid-cols-4"
       >
@@ -118,16 +195,24 @@ export default {
   data() {
     return {
       movies: [],
-      searchMovies: [],
+      searchedMovies: [],
       searchInput: "",
     };
   },
 
   async fetch() {
-    this.searchInput !== ""
-      ? await this.searchedMovies()
-      : await this.getMovies();
-    return;
+    // this.searchInput !== ""
+    //   ? await this.searchedMovies()
+    //   : await this.getMovies();
+    // return;
+
+    if (this.searchInput === "") {
+      await this.getMovies();
+      return;
+    }
+    if (this.searchInput !== "") {
+      await this.searchMovies();
+    }
   },
   methods: {
     async getMovies() {
@@ -139,21 +224,20 @@ export default {
         this.movies.push(movie);
       });
     },
-    async searchedMovies() {
+    async searchMovies() {
       const data = axios.get(
         `https://api.themoviedb.org/3/search/movie?api_key=c3e3b715db7ed78cea000e0969eda5d5&language=en-US&page=1&query=${this.searchInput}`
       );
       const result = await data;
       result.data.results.forEach((movie) => {
-        this.searchMovies.push(movie);
-        return;
+        this.searchedMovies.push(movie);
       });
     },
-    watch: {
-      searchInput() {
-        console.log(this.searchInput);
-      },
-    },
+    // watch: {
+    //   searchInput() {
+    //     console.log(this.searchInput);
+    //   },
+    // },
   },
 };
 </script>
